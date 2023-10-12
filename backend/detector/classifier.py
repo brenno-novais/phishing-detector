7fba8d9444a74cc95150bc98b2b1f8a8edb298ac
Brenno Andrade Novais <brenno133@gmail.com>
2023-10-12 20:15:40 -0300

    [PHIS-002] Cria classe para classificar sites

---
+++ backend/detector/classifier.py
@@ -0,0 +1,41 @@
+import joblib
+
+PHISHING = 1
+LEGITIMATE = 0
+
+
+class ModelLoadingError(Exception):
+    pass
+
+
+class PredictionError(Exception):
+    pass
+
+
+class PhishingClassifier:
+
+    def __init__(self):
+        self.mlp_model = PhishingClassifier.load_mlp_model(
+            "detector\\resources\\mlp_model.pkl")
+
+    @classmethod
+    def load_mlp_model(cls, model_path):
+        try:
+            model = joblib.load(model_path)
+            return model
+        except Exception as e:
+            print(e)
+            raise ModelLoadingError("Failed to load the model") from e
+
+    def classify(self, features):
+        try:
+            prediction = self.mlp_model.predict(features)
+            probabilities = self.mlp_model.predict_proba(features)
+
+            if prediction[0] == 1:
+                return (PHISHING, probabilities)
+            else:
+                return (LEGITIMATE, probabilities)
+        except Exception as e:
+            raise PredictionError(
+                "Error while trying to classify this website.") from e
